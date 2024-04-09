7ea172b204cb5dd56631dd070b189f9d218fdf73
VillafañeNahuel <151423052+Balaturdar@users.noreply.github.com>
2024-04-09 00:58:28 -0300

    modificacion del tp2_1_1 usando punteros

---
+++ tp2_1_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    double *vt;
+    vt =(double*) malloc(sizeof(double) * N);
+
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++){
+        *(vt+1) = 1+rand()%100;
+        printf("%f \n", *(vt+i));
+    }
+
+    return 0;
+}
